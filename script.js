2af5562face8631bc41b77ffffe8e2534f122508
KathrynZh <heying.zhang@torontomu.ca>
2024-11-11 02:11:57 -0500

    integrated js part

--- script.js
+++ script.js
@@ -8,3 +8,36 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+//variable declarations (step 1)//
+var header=document.querySelector("#title-desc");
+var link=document.querySelector(".link");
+var titles=document.querySelectorAll(".article-title");
+
+//event listeners (step 3)//
+setTimeout(contentChange1,5000);
+setTimeout(contentChange2,10000);
+setTimeout(contentChange3,15000);
+link.addEventListener("mouseover",titleBlack);
+link.addEventListener("mouseout",titleRed);
+titles.forEach(function(title) {
+    title.addEventListener("mouseover", titleBlack);
+    title.addEventListener("mouseout", titleRed);
+});
+
+//function definitions (step 2)//
+function contentChange1(){
+    header.textContent = "lets it ride on red.";
+}
+function contentChange2(){
+    header.textContent = "slept here.";
+}
+function contentChange3(){
+    header.textContent = "always leaves room for jello.";
+}
+function titleBlack(){
+    this.style.color="#222";
+}
+function titleRed(){
+    this.style.color = "";
+}
